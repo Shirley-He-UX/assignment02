d69332689e01fcf051b5f13250de82fd9e073a06
Hadesnow <snowbloom1991@gmail.com>
2017-10-12 13:27:48 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,29 +1,29 @@
 function setup() {
-  createCanvas(500, 500);
-  background('#29323c');
+  createCanvas(500,500);
   angleMode(DEGREES);
-  //frameRate(60)
-  }
+ // frameRate(10);
+  background(1);
+}
 
 function draw() {
-
   translate(width/2,height/2);
-  stroke(lerpColor(color('#2196f3'), color('#f44336'), frameCount/60));
-  rotate(frameCount*9);
-  strokeWeight(0.8);
-  line(100,0,100*cos(frameCount*6),-100*sin(frameCount*6));
-  
-  // noFill();  
-  // rotate(frameCount*20);
-  // stroke(lerpColor(color('#fc00ff'), color('#00dbde'), frameCount/80));
-  // rect(220,0,300,500);
-  rotate(frameCount*32);
-  stroke(lerpColor(color(frameCount*2,frameCount,100),color('#f44336'),frameCount/120));
-  //fill(frameCount,frameCount/12,frameCount/2);
   noFill();
-  strokeWeight(2);
-  ellipse(200,frameCount,860,860);
-  if (frameCount == 60) {
+  
+  stroke(lerpColor(color('#fc00ff'), color('#00dbde'), frameCount/90));
+  //bezier(0,250,0,250,250*cos(frameCount*4),-250*sin(frameCount*4),250*cos(frameCount*4),-250*sin(frameCount*4));
+  bezier(0,140,0,0,0,0,140*cos(frameCount*4),-140*sin(frameCount*4));
+
+  rotate(frameCount*60);
+  
+  // rect(-40,0,100,100);
+  
+  // // rotate(frameCount*10);
+  // stroke(lerpColor(color('#fc00ff'), color('#00dbde'), frameCount/60));
+   //ellipse(100,0,200,200);
+
+
+  if (frameCount == 43) {
     noLoop();
   }
+
 }
